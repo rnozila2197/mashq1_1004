36b67b47c629f4f818f4b08152d04abebedb0a6c
rnozila2197 <yjamshid2197@gmail.com>
2025-11-17 16:20:48 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,103 @@
+# #11
+# s = "abcdefhg"
+# print(s)
+#
+# toza = s[0:7:3]
+# print(toza)
+
+# #12
+# s = "QWERTYUIOP"
+# print(s)
+#
+# toza1 = s[::-1]
+# print(toza1)
+#
+# toza2 = s[::-2]
+# print(toza2)
+
+# #13
+# s = "abracadabra"
+# print(s)
+#
+# toza = s[4:8]
+# print(toza)
+
+# #14
+# s = "abcdefghijk"
+# print(s)
+#
+# toza = s[3:3]
+# print(toza)
+
+# #15
+# s = "GameOverMission"
+# print(s)
+#
+# toza = s[4:8]
+# print(toza)
+
+# #16
+# s = "123456789"
+# print(s)
+#
+# toza = s[-5:]
+# print(toza)
+
+# #17
+# s = "indexning"
+# print(s)
+#
+# toza = s[2:5]
+# print(toza)
+
+# #18
+# s = "sliceIsPowerful"
+# print(s)
+#
+# toza = s[3:16]
+# print(toza)
+
+# #19
+# s = "abcdefghij"
+# print(s)
+#
+# toza = s[-5:-2]
+# print(toza)
+
+#20
+s = "ABCDEFGHIJ"
+print(s)
+
+toza = s[-2:-8:-1]
+print(toza)
+
+#21
+s = "mnopqrstuv"
+print(s)
+
+toza = s[0:8]
+print(toza)
+
+#22
+s = "examplestring"
+print(s)
+
+toza1 = s[1:8:6]
+toza2 = s[-4::3]
+print(toza1+toza2)
+
+#23
+s = "123456789abcdef"
+print(s)
+
+toza1 = s[1:6:2]
+toza2 = s[9:14:2]
+print(toza1)
+print(toza2)
+
+#24
+s = "university"
+print(s)
+
+toza = s[3:7]
+print(toza)
